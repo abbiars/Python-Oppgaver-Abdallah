0a6a19f5876378dec3b57a90281e05113333b47e
Abbi <abdallaharson@gmail.com>
2026-01-13 10:59:09 +0100

    Oppgave 1 til 2 ferdig

---
+++ oppgave1.py
@@ -0,0 +1,7 @@
+
+# Spør brukeren om å skrive inn navnet sitt
+navn = input("Skriv navn og etternavn ditt: ")
+
+# Vis velkomstmelding med navnet
+print("Velkommen", navn)
+
---
+++ oppgave2.py
@@ -0,0 +1,9 @@
+# Dette programmet ber brukeren om å skrive inn to tall,
+# legger dem sammen, og viser resultatet.
+a = input("Skriv inn et tall: ") 
+b = input("Skriv inn et annet tall: ")
+# Plusser tallene sammen (Bruker float istedenfor int sånn at det funker med desimaltall også)
+sum = float(a) + float(b)
+
+# Viser summen av tallene
+print("Summen av tallene er:", sum)
