3e8d7307b9166ff7a12b551d2d5d8072bd3d73d4
Abbi <abdallaharson@gmail.com>
2026-01-19 13:01:19 +0100

    jk

--- oppgave2.py
+++ oppgave2.py
@@ -1,6 +1,6 @@
 # Dette programmet ber brukeren om å skrive inn to tall,
 # legger dem sammen, og viser resultatet.
-a = input("Skriv inn et tall: ") 
+a = input("Skriv inn et tall: ")
 b = input("Skriv inn et annet tall: ")
 # Plusser tallene sammen (Bruker float istedenfor int sånn at det funker med desimaltall også)
 sum = float(a) + float(b)
